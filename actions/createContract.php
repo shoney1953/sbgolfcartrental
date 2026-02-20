e426dab2cd885d9952ded830ed5ae14908427cde
sheila honey <sheila_honey_5@hotmail.com>
2026-02-20 11:34:09 -0700

    remove config database 2

--- actions/createContract.php
+++ actions/createContract.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once '../config/Database.php';
+// require_once '../config/Database.php';
 require_once '../models/ContractInfo.php';
 $database = new Database();
 $db = $database->connect();
